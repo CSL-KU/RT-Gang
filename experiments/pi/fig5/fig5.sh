e1d38936d5e17a808fc1fd4b7b931423416cbbf1
Waqar Ali <wali@ku.edu>
2019-02-12 21:36:25 +0000

    rtGang: Verified patch and script on Pi

--- experiments/pi/fig5/fig5.sh
+++ experiments/pi/fig5/fig5.sh
@@ -5,24 +5,38 @@
 # threaded) tasks. Collect traces for kernelshark.
 #==============================================================================
 
+echo 'Solo Experiments'
+chrt -f 5 tau_2 -t 0 -c 2 -n 2 -m 384 -i 85 --jobs 500 --period 30 -v 1 &>     \
+/tmp/tau_2.solo
+sleep 2
+trace-cmd record -e sched_switch chrt -f 10 tau_1 -t 0 -c 0 -n 2 -m 384 -i 43  \
+--jobs 500 --period 20 -v 1 &> /tmp/tau_1.solo
+
+mv trace.dat trace.solo
+mv /tmp/*.solo .
+sleep 5
+
+#==============================================================================
+
 echo 'Co-Sched Experiment'
 echo NO_RT_GANG_LOCK > /sys/kernel/debug/sched_features
 sleep 2
 
 tau_be_mem -t 0 -c 2 -n 1 -m 1024 -i 1000000000 --jobs 100000 --period 0 &>    \
-tau_be_mem.mint &
+/tmp/tau_be_mem.mint &
 tau_be_cpu -t 0 -c 3 -n 1 -m 8 -i 1000000000 --jobs 100000 --period 0 &>       \
-tau_be_cpu.mint &
+/tmp/tau_be_cpu.mint &
 
 chrt -f 5 tau_2 -t 0 -c 2 -n 2 -m 384 -i 85 --jobs 500 --period 30 -v 1 &>     \
-tau_2.mint &
-trace-cmd record -e sched_switch chrt -f 10 tau_1 -t 0 -c 0 -n 2 -m 384 -i 50  \
---jobs 500 --period 20 -v 1 &> tau_1.mint
+/tmp/tau_2.mint &
+trace-cmd record -e sched_switch chrt -f 10 tau_1 -t 0 -c 0 -n 2 -m 384 -i 43  \
+--jobs 500 --period 20 -v 1 &> /tmp/tau_1.mint
 
 killall -s SIGTERM tau_2
 killall -s SIGTERM tau_be_mem
 killall -s SIGTERM tau_be_cpu
 mv trace.dat trace.mint
+mv /tmp/*.mint .
 sleep 5
 
 #==============================================================================
@@ -33,25 +47,20 @@ insmod ../../../throttling/kernel_module/exe/bwlockmod.ko
 sleep 2
 
 tau_be_mem -t 0 -c 2 -n 1 -m 1024 -i 1000000000 --jobs 100000 --period 1000 &> \
-tau_be_mem.rtg &
+/tmp/tau_be_mem.rtg &
 tau_be_cpu -t 0 -c 3 -n 1 -m 8 -i 1000000000 --jobs 100000 --period 1000 &>    \
-tau_be_cpu.rtg &
+/tmp/tau_be_cpu.rtg &
 
-chrt -f 5 tau_2 -t 0 -c 2 -n 2 -m 384 -i 75 --jobs 500 --period 30 -v 1 &>     \
-tau_2.rtg &
-pid2=$!
-trace-cmd record -e sched_switch chrt -f 10 tau_1 -t 0 -c 0 -n 2 -m 384 -i 50  \
---jobs 500 --period 20 -v 1 &> tau_1.rtg &
-pid1=$!
+chrt -f 5 tau_2 -t 0 -c 2 -n 2 -m 384 -i 85 --jobs 500 --period 30 -v 1 &>     \
+/tmp/tau_2.rtg &
+trace-cmd record -e sched_switch chrt -f 10 tau_1 -t 0 -c 0 -n 2 -m 384 -i 43  \
+--jobs 500 --period 20 -v 1 &> /tmp/tau_1.rtg
 
-./../../../throttling/user_app/test -p ${pid1} -v 1 -e 1638
-./../../../throttling/user_app/test -p ${pid2} -v 1 -e 1638
-
-wait ${pid1}
 killall -s SIGTERM tau_2
 killall -s SIGTERM tau_be_mem
 killall -s SIGTERM tau_be_cpu
 mv trace.dat trace.rtg
+mv /tmp/*.rtg .
 sleep 5
 
 rmmod bwlockmod
--- throttling/user_app/test.c
+++ throttling/user_app/test.c
@@ -7,7 +7,7 @@
 #include <sys/syscall.h>
 #include <time.h>
 
-#define SYS_bwlock 245
+#define SYS_bwlock 255
 
 int main(int argc, char *argv [])
 {
